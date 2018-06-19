30bff68302b0f218125f65330abb85fe3a233b2e
hiroyuki yamaki <uu137582@RLSUU137582M01.local>
2018-06-19 15:53:47 +0900

    implement compare images script

--- compare.sh
+++ compare.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-for i in $(ls); do
-	echo $i;
+#
+# Usage: compare.sh {ordSrcDir} {newSrcDir} {destDir}
+#
+oldSrcDir=$1
+newSrcDir=$2
+destDir=$3
+
+for image in $(ls $oldSrcDir); do
+	echo "$image will be compared.";
+	composite -compose difference $oldSrcDir/$image $newSrcDir/$image $destDir/$image
+	existsDiff=$(identify -format "%[mean]" $destDir/$image)
+	echo "diff exists?: $existsDiff"
+	if [ $existsDiff = "0" ]; then
+	    echo "diff not exists."
+		rm $destDir/$image
+	else
+	    echo "diff exists."
+		compare -fuzz 2% -metric AE $oldSrcDir/$image $newSrcDir/$image $destDir/$image
+		convert +append $oldSrcDir/$image $newSrcDir/$image $destDir/$image $destDir/$image
+	fi	
 done
 
 
